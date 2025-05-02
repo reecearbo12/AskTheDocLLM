3960af4821e2dbf79642c8ddc2bdbc86990b6171
reecearbo12 <118308765+reecearbo12@users.noreply.github.com>
2025-05-02 18:41:40 -0500

    Add files via upload

--- AskTheDoc.py
+++ AskTheDoc.py
@@ -8,7 +8,7 @@ import streamlit as st
 from langchain.llms import OpenAI
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import Chroma
+from langchain.vectorstores import FAISS
 from langchain.chains import RetrievalQA
 
 def generate_response(uploaded_file, openai_api_key, query_text):
@@ -21,7 +21,7 @@ def generate_response(uploaded_file, openai_api_key, query_text):
         # Select embeddings
         embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
         # Create a vectorstore from documents
-        db = Chroma.from_documents(texts, embeddings)
+        db = FAISS.from_documents(texts, embeddings)
         # Create retriever interface
         retriever = db.as_retriever()
         # Create QA chain
